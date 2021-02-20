268b90e5ff4c27401b20814a9fbf21b6d1c27e52
dinesh-more <moredinesh93@gmail.com>
2021-02-20 14:59:34 +0530

    Initial Commit

---
+++ src/main/java/com/jwt/model/JwtResponse.java
@@ -0,0 +1,25 @@
+package com.jwt.model;
+
+public class JwtResponse {
+
+	private String jwtToken;
+
+	public JwtResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JwtResponse(String jwtToken) {
+		super();
+		this.jwtToken = jwtToken;
+	}
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+
+}
